66ec30dbc9d42b393a2f81b3dd9a3b418c8ed592
MrSuicideFish <malcolm259@gmail.com>
2024-01-03 16:19:09 -0800

    Mouse indicator on objects now fades in and out

--- In the Shadow of the Sun/Assets/Source/Gameplay/Entity/ClickableEntity.cs
+++ In the Shadow of the Sun/Assets/Source/Gameplay/Entity/ClickableEntity.cs
@@ -1,10 +1,38 @@
+using System;
+using DG.Tweening;
 using UnityEngine;
 
 public class ClickableEntity : MonoBehaviour
 {
     public GameObject view;
+    public FadeController indicator;
     
     protected virtual void OnSelected(){}
+
+    private void Start()
+    {
+        indicator.Opacity = 0;
+    }
+
+    private void OnMouseOver()
+    {
+        if (view.activeInHierarchy)
+        {
+            DOTween.To(
+                () => indicator.Opacity,
+                (x) => indicator.Opacity = x,
+                1.0f, 1.0f);    
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        DOTween.To(
+            () => indicator.Opacity,
+            (x) => indicator.Opacity = x,
+            0.0f, 1.0f);
+    }
+
     private void OnMouseDown()
     {
         Debug.Log("Selected World Entity");
